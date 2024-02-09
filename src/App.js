3c2abcb09e5e84aa6fc5bc86b85adfda6569ffa2
Ryichi <richard4pech@gmail.com>
2024-02-10 01:21:02 +1100

    Add miku

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import miku from './mikuheart.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editing <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <b>Hey, I'm Richard!</b>
+        <p>It's nice to meet you.</p>
+        <img src={miku} className="miku-cute" alt="miku" width={500}/>
       </header>
     </div>
   );
